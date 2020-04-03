5405ddec41fb3a3cdb03029409220fa3d3e92d1f
Berni <bernhard.hansemann@gmail.com>
2020-04-03 21:14:13 +0200

    Added Inputs

--- engine_calc_adiabatic.py
+++ engine_calc_adiabatic.py
@@ -1,2 +1,10 @@
-#adiabatic approximation of thermodynamic properties inside the engine
+# adiabatic approximation of thermodynamic properties inside the engine
 
+# inputs
+p_ch = 100000       # chamber pressure in Pa
+T_ch = 2500         # chamber temperature in Kelvin
+F_thrust = 500      # Thrust in N
+x_n2 = 0.45         #Molar fraction of exhaust products, via NASA CEA
+x_co = 0.15
+x_co2 = 0.2
+x_h20 = 0.2
