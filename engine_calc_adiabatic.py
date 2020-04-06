af22420f4a4c7c0be642daca99ca8c1c0466ec86
BHansemann <bernhard.hansemann@gmail.com>
2020-04-06 14:10:24 +0200

    Correct mole fractions

--- engine_calc_adiabatic.py
+++ engine_calc_adiabatic.py
@@ -2,9 +2,12 @@
 
 # inputs
 p_ch = 100000       # chamber pressure in Pa
+p_e = 10000         # exit pressure in Pa
 T_ch = 2500         # chamber temperature in Kelvin
 F_thrust = 500      # Thrust in N
-x_n2 = 0.45         #Molar fraction of exhaust products, via NASA CEA
-x_co = 0.15
-x_co2 = 0.2
-x_h20 = 0.2
+x_n2 = 0.34269      # Mole fraction of exhaust products, via NASA CEA
+x_co = 0.23055
+x_h2 = 0.21104
+x_h20 = 0.17950
+x_co2 = 0.03140
+x_h = 0.00482
